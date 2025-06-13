8a01e59a3bc44526657354f642cc03e10a3dd72c
Jesse Wang <z872845991@163.com>
2025-06-13 17:03:03 +0800

    update zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,3 +6,7 @@ alias ts="trans :zh -shell"
 alias xp="xclip -selection clipboard"
 alias lls="logo-ls"
 alias td="todoist-cli q"
+unalias z
+z() {
+  cd "$(zshz -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+}
