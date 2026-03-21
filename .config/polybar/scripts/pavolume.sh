34c0644ccfcbe410a56a122cd8481d4ec1604834
Jesse Wang <z872845991@gmail.com>
2026-03-22 02:19:23 +0800

    fix(polybar): audio config

--- .config/polybar/scripts/pavolume.sh
+++ .config/polybar/scripts/pavolume.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 
-# finds the active sink for pulse audio and increments the volume. useful when you have multiple audio outputs and have a key bound to vol-up and down
-
 osd='no'
 inc='2'
 capvol='no'
 maxvol='200'
 autosync='yes'
 
-# Muted status
-# yes: muted
-# no : not muted
 curStatus="no"
 active_sink=""
 limit=$((100 - inc))
 maxlimit=$((maxvol - inc))
 
+# 重新获取默认 Sink 的名称（PipeWire 下推荐用名称而非索引）
 reloadSink() {
-    active_sink=$(pacmd list-sinks | awk '/\* index:/{print $3}')
+    active_sink=$(pactl get-default-sink)
 }
 
 function volUp {
-
     getCurVol
-
     if [ "$capvol" = 'yes' ]
     then
         if [ "$curVol" -le 100 ] && [ "$curVol" -ge "$limit" ]
         then
-            pactl set-sink-volume "$active_sink" -- 100%
+            pactl set-sink-volume "$active_sink" 100%
         elif [ "$curVol" -lt "$limit" ]
         then
-            pactl set-sink-volume "$active_sink" -- "+$inc%"
+            pactl set-sink-volume "$active_sink" "+$inc%"
         fi
     elif [ "$curVol" -le "$maxvol" ] && [ "$curVol" -ge "$maxlimit" ]
     then
@@ -42,51 +36,36 @@ function volUp {
     fi
 
     getCurVol
-
-    if [ ${osd} = 'yes' ]
-    then
-        qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
-    fi
-
-    if [ ${autosync} = 'yes' ]
-    then
-        volSync
-    fi
+    [ "${osd}" = 'yes' ] && qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
+    [ "${autosync}" = 'yes' ] && volSync
 }
 
 function volDown {
-
     pactl set-sink-volume "$active_sink" "-$inc%"
     getCurVol
-
-    if [ ${osd} = 'yes' ]
-    then
-        qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
-    fi
-
-    if [ ${autosync} = 'yes' ]
-    then
-        volSync
-    fi
-
+    [ "${osd}" = 'yes' ] && qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
+    [ "${autosync}" = 'yes' ] && volSync
 }
 
+# 获取当前 Sink 的所有输入流
 function getSinkInputs {
-    input_array=$(pacmd list-sink-inputs | grep -B 4 "sink: $1 " | awk '/index:/{print $2}')
+    input_array=$(pactl list sink-inputs short | awk '{print $1}')
 }
 
 function volSync {
-    getSinkInputs "$active_sink"
+    getSinkInputs
     getCurVol
-
     for each in $input_array
     do
         pactl set-sink-input-volume "$each" "$curVol%"
     done
 }
 
+# 核心修复：使用 pactl 解析当前音量
 function getCurVol {
-    curVol=$(pacmd list-sinks | grep -A 15 "index: $active_sink$" | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')
+    # 提取百分比数字
+    curVol=$(pactl get-sink-volume "$active_sink" | grep -Po '[0-9]+(?=%)' | head -n 1)
+    [ -z "$curVol" ] && curVol=0
 }
 
 function volMute {
@@ -102,46 +81,22 @@ function volMute {
             status=0
             ;;
     esac
-
-    if [ ${osd} = 'yes' ]
-    then
-        qdbus org.kde.kded /modules/kosd showVolume ${curVol} ${status}
-    fi
-
+    [ "${osd}" = 'yes' ] && qdbus org.kde.kded /modules/kosd showVolume ${curVol} ${status}
 }
 
+# 核心修复：使用 pactl 获取静音状态
 function volMuteStatus {
-    curStatus=$(pacmd list-sinks | grep -A 15 "index: $active_sink$" | awk '/muted/{ print $2}')
+    curStatus=$(pactl get-sink-mute "$active_sink" | awk '{print $2}')
 }
 
-# Prints output for bar
-# Listens for events for fast update speed
 function listen {
     firstrun=0
-
-    pactl subscribe 2>/dev/null | {
-        while true; do
-            {
-                # If this is the first time just continue
-                # and print the current state
-                # Otherwise wait for events
-                # This is to prevent the module being empty until
-                # an event occurs
-                if [ $firstrun -eq 0 ]
-                then
-                    firstrun=1
-                else
-                    read -r event || break
-                    if ! echo "$event" | grep -e "on card" -e "on sink"
-                    then
-                        # Avoid double events
-                        continue
-                    fi
-                fi
-            } &>/dev/null
+    # PipeWire 同样支持 pactl subscribe
+    pactl subscribe 2>/dev/null | while read -r event; do
+        if echo "$event" | grep -qE "sink|server"; then
             output
-        done
-    }
+        fi
+    done
 }
 
 function output() {
@@ -154,41 +109,22 @@ function output() {
     else
         echo " $curVol%"
     fi
-} #}}}
+}
 
 reloadSink
 case "$1" in
-    --up)
-        volUp
-        ;;
-    --down)
-        volDown
-        ;;
+    --up) volUp ;;
+    --down) volDown ;;
     --togmute)
         volMuteStatus
-        if [ "$curStatus" = 'yes' ]
-        then
-            volMute unmute
-        else
-            volMute mute
-        fi
+        if [ "$curStatus" = 'yes' ]; then volMute unmute; else volMute mute; fi
         ;;
-    --mute)
-        volMute mute
+    --mute) volMute mute ;;
+    --unmute) volMute unmute ;;
+    --sync) volSync ;;
+    --listen) 
+        output # 先打印一次当前状态
+        listen 
         ;;
-    --unmute)
-        volMute unmute
-        ;;
-    --sync)
-        volSync
-        ;;
-    --listen)
-        # Listen for changes and immediately create new output for the bar
-        # This is faster than having the script on an interval
-        listen
-        ;;
-    *)
-        # By default print output for bar
-        output
-        ;;
+    *) output ;;
 esac
